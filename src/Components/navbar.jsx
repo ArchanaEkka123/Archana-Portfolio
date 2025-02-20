ff2220c524d043be30a807e8f684ea7a69af1777
archana ekka <archanaekka97555@gmail.com>
2025-02-20 15:50:08 +0530

    first commit

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,44 @@
+
+import { Link } from 'react-router-dom';
+import './navbar.scss'; // Optional for styling
+
+const Navbar = () => {
+  return (
+   
+<div className="nav">
+                <nav className="navbar">
+               
+              <img
+                class="nav_icon"
+                src="/images/A_logo.svg"
+                alt="Description of the image"
+                width={90}
+                height={90}
+              />
+                 <ul className='nav_ul'>
+                
+                  <li><Link to="/"  className='nav_content' >Home</Link>
+
+                  </li>
+                  <li><Link to="/about" className='nav_content'>About</Link>
+
+                  </li>
+                  <li>
+                  <Link to="/services" className='nav_content'>Services</Link>
+
+                  </li>
+                  <li><Link to="/contact" className='nav_content'> Contact</Link>
+                   
+                  </li>
+                  <li><Link to="/ImageSlider" className='nav_content'> Gallery</Link>
+                   
+                  </li>
+                </ul>
+              </nav>
+              
+              </div> 
+
+  );
+};
+
+export default Navbar;
